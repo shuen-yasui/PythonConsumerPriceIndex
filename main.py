0fe6c3982e61374f9d720b52b78ab138dfa18076
Shuen Yasui <shuenyasui@gmail.com>
2021-06-06 19:58:56 -0700

    Removed non-data rows from csv. Fixed drop col lines. Added second line to plot

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from bokeh.models import ColumnDataSource
 
 # Extract data from csv
 df=pd.read_csv("data.csv", index_col=2)
+print(df)
 df=df.transpose()
 # Drop cols and rows for debugging
 df=df.drop(df.index[range(3)])
-df.drop(df.columns[2:], axis = 1, inplace = True)
-df.drop(df.columns[0], axis = 1, inplace = True)
 # Reset DF index
 df=df.reset_index(drop=True)
 print(df)
 # Create Bokeh plot
 sc=ColumnDataSource(df)
 p=figure(y_range=(0,200))
-p.line(x='index', y='Canada', source=sc, line_width=2)
-#p.vbar(x='index', top='Canada', source=sc, width=0.7)
+p.line(x='index', y='Canada', line_color="blue", source=sc, line_width=2)
+p.line(x='index', y='Switzerland', line_color="red", source=sc, line_width=2)
 show(p)
