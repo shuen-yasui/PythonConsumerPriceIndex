dd6ed92d508df227b62602b8b23cfa3d7b902f9a
Shuen Yasui <shuenyasui@gmail.com>
2021-06-06 20:49:55 -0700

    Add line plot for each country. X-Axis now displays years. Line plot rotates through colors and line styles

--- main.py
+++ main.py
@@ -13,7 +13,21 @@ df=df.reset_index(drop=True)
 print(df)
 # Create Bokeh plot
 sc=ColumnDataSource(df)
-p=figure(y_range=(0,200))
-p.line(x='index', y='Canada', line_color="blue", source=sc, line_width=2)
-p.line(x='index', y='Switzerland', line_color="red", source=sc, line_width=2)
+p=figure(y_range=(0,200), width=1500)
+# arrays of colors and line styles
+colors=['red','green','blue','purple','orange','lightblue','lightgreen','violet','black']
+lineStyles=['solid','dashed','dotted']
+iColor=0
+iLine=0
+# create years column
+years=list(range(1960,2021))
+sc.data['years']=years
+# For each country create line plot using designated color and style
+for country in df.columns:
+    p.line(x='years', y=country, line_color=colors[iColor], line_dash=lineStyles[iLine], source=sc, line_width=2, legend_label=country)
+    # cycle through colors and increment style one out of color
+    iColor+=1
+    if iColor>=len(colors):
+        iColor=0
+        iLine+=1
 show(p)
